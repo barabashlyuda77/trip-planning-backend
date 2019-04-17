52a75d82fb9cd848b32b951e54f9f1628ccaa421
Lyuda Barabash <barabashlyuda@gmail.com>
2019-04-17 12:13:11 +0700

    Set up backend for trip-planing

---
+++ src/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const db = [];
+
+app.post('/add-data-to-db/', (req, res) => {
+  db.push(req.body);
+  res.sendStatus(200);
+});
+
+app.listen(8000, () => console.log('App is running'));
